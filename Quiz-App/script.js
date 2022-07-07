2b35a5c7240b4440b49a51f328ff9703c565e74d
Yunus Emre Adaş <yunusemre.adas@stu.fsm.edu.tr>
2022-07-07 22:38:59 +0300

    Card script and design

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -50,11 +50,8 @@ Soru.prototype.cevabKontrol = function (cevap) { //Metodu ithiyaca binaen kullan
     return cevap === this.dogruCevap;
 }
 
-let soru1 = new Soru("Java paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "c");
-let soru2 = new Soru(".Net paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "b");
-
 let sorular = [
-    new Soru("1-Java paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "c"),
+    new Soru("1-Java paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm", d:"Java" }, "c"),
     new Soru("2-.Net paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "b"),
     new Soru("3-Python paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "c"),
     new Soru("4-Java paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "c")
@@ -76,11 +73,35 @@ const quiz = new Quiz(sorular);
 
 document.querySelector(".btn-start").addEventListener("click", function () {
     if (quiz.sorular.length != quiz.soruIndex) {
-        console.log(quiz.soruGetir());
+        document.querySelector(".quiz_box").classList.add("active");
+        soruGoster(quiz.soruGetir());
+    }
+});
+
+document.querySelector(".next-btn").addEventListener("click", function(){
+    //yazılan css buton aktif edilirse aktif oluyor. Ters style active class olarak ekleniyor!!
+    if (quiz.sorular.length != quiz.soruIndex+1) {
         quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
     }else{
+        alert("Quiz Bitti");
         console.log("Quiz bitti aq!")
     }
     
 });
 
+function soruGoster(soru){
+    let question = `<span class="question">${soru.soruMetni}</span>`;
+    let option = '';
+
+    for(let cevap in soru.cevaplar){
+        option +=
+        `<div class="option">
+            <span><b>${cevap}</b>:${soru.cevaplar[cevap]}</span>
+            <div class="icon"><i class="fas fa-times"></i></div>
+        </div>`;
+    }
+
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = option;
+}
