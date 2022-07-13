ca319a1fcf4193ebd07c3cd263f6e42b7d76c486
Yunus Emre Adaş <yunusemre.adas@stu.fsm.edu.tr>
2022-07-13 19:06:03 +0300

    Son düzenleme.

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -1,52 +1,45 @@
-
 const quiz = new Quiz(sorular);
 const ui = new UI();
 
-
-    ui.btn_start.addEventListener("click", function () {
-    if (quiz.sorular.length != quiz.soruIndex) {
-        ui.quiz_box.classList.add("active");
-        ui.soruGoster(quiz.soruGetir());
-        ui.soruSayisiGoster(quiz.soruIndex + 1, quiz.sorular.length);
-        ui.next_btn.classList.remove("show");
-    }
+ui.btn_start.addEventListener("click", function () {
+  if (quiz.sorular.length != quiz.soruIndex) {
+    ui.quiz_box.classList.add("active");
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.next_btn.classList.remove("show");
+  }
 });
 
-
-
-    ui.next_btn.addEventListener("click", function () {
-    //yazılan css buton aktif edilirse aktif oluyor. Ters style active class olarak ekleniyor!!
-    if (quiz.sorular.length != quiz.soruIndex + 1) {
-        quiz.soruIndex += 1;
-        ui.soruGoster(quiz.soruGetir());
-        ui.soruSayisiGoster(quiz.soruIndex + 1, quiz.sorular.length);
-        ui.next_btn.classList.remove("show");
-    } else {
-        alert("Quiz Bitti");
-        console.log("Quiz bitti aq!")
-    }
-
+ui.next_btn.addEventListener("click", function () {
+  //yazılan css buton aktif edilirse aktif oluyor. Ters style active class olarak ekleniyor!!
+  if (quiz.sorular.length != quiz.soruIndex + 1) {
+    quiz.soruIndex += 1;
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.next_btn.classList.remove("show");
+  } else {
+    alert("Quiz Bitti");
+    console.log("Quiz bitti aq!");
+  }
 });
-
-
 
 function optionSelected(optionList) {
-    let cevap = optionList.querySelector("span b").textContent;
-    console.log(cevap);
+  let cevap = optionList.querySelector("span b").textContent;
+  console.log(cevap);
 
-    let soru = quiz.soruGetir();
+  let soru = quiz.soruGetir();
 
-    if (soru.cevabKontrol(cevap)) {
-        optionList.classList.add("correct");
-        optionList.insertAdjacentHTML("beforeend", ui.correctIcon);
-    } else {
-        optionList.classList.add("incorrect");
-        optionList.insertAdjacentHTML("beforeend", ui.incorrectIcon);
-    }
+  if (soru.cevabKontrol(cevap)) {
+    optionList.classList.add("correct");
+    optionList.insertAdjacentHTML("beforeend", ui.correctIcon);
+  } else {
+    optionList.classList.add("incorrect");
+    optionList.insertAdjacentHTML("beforeend", ui.incorrectIcon);
+  }
 
-    for (i = 0; i < ui.option_list.children.length; i++) {
-        ui.option_list.children[i].classList.add("disabled");
-    }
+  for (i = 0; i < ui.option_list.children.length; i++) {
+    ui.option_list.children[i].classList.add("disabled");
+  }
 
-    ui.next_btn.classList.add("show");
+  ui.next_btn.classList.add("show");
 }
--- Quiz-App/soru.js
+++ Quiz-App/soru.js
@@ -1,16 +1,32 @@
 function Soru(soruMetni, cevaplar, dogruCevap) {
-    this.soruMetni = soruMetni;
-    this.cevaplar = cevaplar;
-    this.dogruCevap = dogruCevap;
-
+  this.soruMetni = soruMetni;
+  this.cevaplar = cevaplar;
+  this.dogruCevap = dogruCevap;
 }
-Soru.prototype.cevabKontrol = function (cevap) { //Metodu ithiyaca binaen kullanmak için prototype içine atma yöntemi!!!
-    return cevap === this.dogruCevap;
-}
+Soru.prototype.cevabKontrol = function (cevap) {
+  //Metodu ithiyaca binaen kullanmak için prototype içine atma yöntemi!!!
+  return cevap === this.dogruCevap;
+};
 
 let sorular = [
-    new Soru("1-Java paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm", d: "Java" }, "c"),
-    new Soru("2-.Net paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "b"),
-    new Soru("3-Python paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "c"),
-    new Soru("4-Java paket yönetimi uygulaması nedir?", { a: "Node.js", b: "nuget", c: "Npm" }, "c")
-]
+  new Soru(
+    "1-Java paket yönetimi uygulaması nedir?",
+    { a: "Node.js", b: "nuget", c: "Npm", d: "Java" },
+    "c"
+  ),
+  new Soru(
+    "2-.Net paket yönetimi uygulaması nedir?",
+    { a: "Node.js", b: "nuget", c: "Npm" },
+    "b"
+  ),
+  new Soru(
+    "3-Python paket yönetimi uygulaması nedir?",
+    { a: "Node.js", b: "nuget", c: "Npm" },
+    "c"
+  ),
+  new Soru(
+    "4-Java paket yönetimi uygulaması nedir?",
+    { a: "Node.js", b: "nuget", c: "Npm" },
+    "c"
+  ),
+];
